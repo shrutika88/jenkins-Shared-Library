0d9555fe758ebc732efc54c7b2af85fad8fb54a0
shrutika88 <43811721+shrutika88@users.noreply.github.com>
2018-10-17 17:55:56 +0530

    Update sayHello.groovy

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -1,6 +1,7 @@
 /**
 * Hello world step.
 */
-def call(String name) {   
-   echo "Hello $name!"
+def call(String repo,String branch) {   
+   checkout([$class: 'GitSCM', branches: [[name: '*/master']],
+     userRemoteConfigs: [[url: 'http://git-server/user/repository.git']]])
 }
