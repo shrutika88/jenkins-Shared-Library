787fb98500d4f887865db97935d2569f884ed6f7
shrutika88 <43811721+shrutika88@users.noreply.github.com>
2018-10-17 18:12:32 +0530

    Update gitCheckout.groovy

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -2,6 +2,6 @@
 * Hello world step.
 */
 def call(String repo,String branch) {   
-   checkout([$class: 'GitSCM', branches: [[name: '*/$repo']],
-     userRemoteConfigs: [[url: '$branch']]])
+   checkout([$class: 'GitSCM', branches: [[name: '*/$branch']],
+     userRemoteConfigs: [[url: '$repo']]])
 }
