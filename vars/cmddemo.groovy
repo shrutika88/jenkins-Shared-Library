9d56015a221015a1fa96ce3beb0d2b05d22028f3
shrutika88 <43811721+shrutika88@users.noreply.github.com>
2018-10-19 12:19:42 +0530

    Update cmddemo.groovy

--- vars/cmddemo.groovy
+++ vars/cmddemo.groovy
@@ -1,3 +1,6 @@
+@Grab('org.apache.commons:commons-lang3:3.4+')
+import org.apache.commons.lang.time.StopWatch
+
 def call (String cmdToRun) {
   def sw = new StopWatch()      
     def proc = "$cmdToRun".execute()      
