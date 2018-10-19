e51483c8050c57f7c64cb2db0810ccb6586620c8
shrutika88 <43811721+shrutika88@users.noreply.github.com>
2018-10-19 17:33:47 +0530

    Update cmddemo.groovy

--- vars/cmddemo.groovy
+++ vars/cmddemo.groovy
@@ -1,3 +1,3 @@
 def call(String cmd) {
-    sh("$cmd")
+    checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'git_creds', url: 'https://github.com/DSLtesting/OriginalRepo.git']]])
 }
