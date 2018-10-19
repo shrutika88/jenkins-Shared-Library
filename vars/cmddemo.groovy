224610e7fcc4cee19a857fd830c7d74f2a8879e0
shrutika88 <43811721+shrutika88@users.noreply.github.com>
2018-10-19 17:30:04 +0530

    Update cmddemo.groovy

--- vars/cmddemo.groovy
+++ vars/cmddemo.groovy
@@ -1,9 +1,3 @@
-def call(body) {
-    // evaluate the body block, and collect configuration into the object
-    def config = [:]
-    body.resolveStrategy = Closure.DELEGATE_FIRST
-    body.delegate = config
-    body()
-
-    sh "git clone https://github.com/shrutika88/final_demo.git"
+def call(String cmd) {
+    sh("$cmd")
 }
