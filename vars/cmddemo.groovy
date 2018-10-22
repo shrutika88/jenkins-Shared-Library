6b1d3e9765a72e33bf8b51da0ec5f66456034bf2
shrutika88 <43811721+shrutika88@users.noreply.github.com>
2018-10-22 16:23:32 +0530

    Update cmddemo.groovy

--- vars/cmddemo.groovy
+++ vars/cmddemo.groovy
@@ -1,3 +1,3 @@
-def call() {
-    checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'git_creds', url: 'https://github.com/shrutika88/OriginalRepo.git']]])
+def call(String repo) {
+    checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'git_creds', url:"$repo"]]])
 }
